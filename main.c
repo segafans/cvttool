4ad41e6f68e761bac8e0d0709cd2664fa1124a93
segafans <segafans132@gmail.com>
2014-06-10 11:37:10 +0800

    easy parse ok

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <stdarg.h>
 
 #include "list.h"
 
@@ -26,6 +27,8 @@
 #define BITMAP_CLR( bitmap, bit )  (bitmap[BITMAP_INDEX(bit)]&=~BITMAP_MASK(bit))
 #define BITMAP_SET( bitmap, bit )  (bitmap[BITMAP_INDEX(bit)]|=BITMAP_MASK(bit))
 
+#define __ printf
+
 /*---------------------------- Type define ------------------------------*/
 typedef enum {
     TYPE_STRING,
@@ -42,6 +45,7 @@ typedef struct {
 } T_Value;
 
 typedef void (*FNC_DEBUG)(T_Value *ptValue);
+static int f_Level = 0;
 
 /*---------------------- Local function declaration ---------------------*/
 static int locParse(char *psBuf, H_LIST hList);
@@ -49,12 +53,15 @@ static T_Value * valueNew(E_TYPE eType, char *psValue, int iLen);
 
 static void valueStringDebug(T_Value *ptValue);
 static void valueBitMapDebug(T_Value *ptValue);
-static int printfChar(unsigned char caChar);
+static int storeChar(unsigned char caChar, unsigned char *psFrist, unsigned char *psLast);
 static char * getBitMap(char *psBuf, int *piLen);
 static void valueNodeStartDebug(T_Value *ptValue);
 static void valueNodeEndDebug(T_Value *ptValue);
 static char * getName(char *psBuf, int *piLen);
 static int getLoop(char *psBuf, int *piLen);
+static int printfChar(unsigned char caFrist, unsigned char caLast, int *piFlag);
+static int _s();
+static int _(char *psBuf, ...);
 
 /*-------------------------  Global variable ----------------------------*/
 FNC_DEBUG f_fncDebug[TYPE_NUM] = {
@@ -70,9 +77,25 @@ int main(int argc, char *argv[])
 {
     H_LIST hList = listNew();
 
-    locParse("1(?:<test>23456)*[a-z1-9A\\-Z]*890.{10}", hList);
+    locParse("1(?:<test>23456)*[a-z]*890.{10}", hList);
 
+    _("#include <string.h>");
+    _("#include <stdio.h>");
+    _("");
+    
+    _("int locParse(char *psBuf)");
+    _("{");
+    f_Level = 1;
+    _("int iPos = 0;");
     LIST_LOOP(hList, f_fncDebug[((T_Value *)ptIter)->eType](ptIter));
+    _("return 0;");
+    f_Level = 0;
+    _("}");
+
+    _("");
+    _("int main(int argc, char *argv[]) {");
+    _("    printf(\"%%d\\n\", locParse(\"123456a8901a\"));");
+    _("}");
 
     return 0;
 }
@@ -137,7 +160,7 @@ static int locParse(char *psBuf, H_LIST hList)
                 pCur += iLen + 4;
             }
         } else if (')' == caTemp) {
-            listAdd(hList, valueNew(TYPE_NODE_END, NULL, 0));
+            listAdd(hList, valueNew(TYPE_NODE_END, ptLastNode->psValue, -1));
             ptLast = ptLastNode;
             ptLastNode = NULL;
         }
@@ -234,6 +257,10 @@ static T_Value * valueNew(E_TYPE eType, char *psValue, int iLen)
     ptValue->eType = eType;
     ptValue->iLoop = 1;
 
+    if (iLen < 0) {
+        iLen = strlen(psValue);
+    }
+
     ptValue->psValue = NULL;
     if (NULL != psValue && 0 != iLen) {
         ptValue->psValue = malloc(iLen+1);
@@ -246,68 +273,125 @@ static T_Value * valueNew(E_TYPE eType, char *psValue, int iLen)
 
 static void valueStringDebug(T_Value *ptValue)
 {
-    printf("[string][%d]%s\n", ptValue->iLoop, ptValue->psValue);
+    _("/* [string][%d]%s */", ptValue->iLoop, ptValue->psValue);
+    _("if (0 != memcmp(psBuf+iPos, \"%s\", %ld)) {", ptValue->psValue, strlen(ptValue->psValue));
+    _("    return -1;");
+    _("}");
+    _("iPos += %ld;", strlen(ptValue->psValue));
+    _("");
 }
 
 static void valueNodeStartDebug(T_Value *ptValue)
 {
-    printf("[node start][%d]", ptValue->iLoop);
-    if (NULL != ptValue->psValue) {
-        printf("%s", ptValue->psValue);
-    }
+    _("/* [node start][%d]%s */", ptValue->iLoop, ptValue->psValue);
 
-    printf("\n");
+    _("char *s%s = psBuf+iPos;", ptValue->psValue);
+    _("");
 }
 
 static void valueNodeEndDebug(T_Value *ptValue)
 {
-    printf("[node end]\n");
+    _("/* [node end] %s*/", ptValue->psValue);
+
+    _("int i%sLen = psBuf + iPos - s%s;", ptValue->psValue, ptValue->psValue);
+    _("");
 }
 
 static void valueBitMapDebug(T_Value *ptValue)
 {
-    printf("[bitmap][%d]", ptValue->iLoop);
+    unsigned char caFrist;
+    unsigned char caLast;
+    _s();__("/* [bitmap][%d]", ptValue->iLoop);
 
     unsigned int i = 0;
     for (i=1; i<=128; i++) {
         if (BITMAP_TEST(ptValue->psValue, i)) {
-            printfChar(i);
+            storeChar(i, NULL, NULL);
         } else {
-            printfChar(0);
+            storeChar(0, &caFrist, &caLast);
+            if ('\0' != caFrist) {
+                if ('\0' == caLast) {
+                    __("0x%03d", caFrist);
+                } else {
+                    __("0x%03d-0x%03d", caFrist, caLast);
+                }
+            }
         }
     }
-    printfChar(0);
+    storeChar(0, &caFrist, &caLast);
+    if ('\0' != caFrist) {
+        if ('\0' == caLast) {
+            __("0x%03d", caFrist);
+        } else {
+            __("0x%03d-0x%03d", caFrist, caLast);
+        }
+    }
 
-    printf("\n");
+    __(" */\n");
+
+    _s();__("if (");
+    int iFlag = 0;
+    for (i=1; i<=128; i++) {
+        if (BITMAP_TEST(ptValue->psValue, i)) {
+            storeChar(i, NULL, NULL);
+        } else {
+            storeChar(0, &caFrist, &caLast);
+            printfChar(caFrist, caLast, &iFlag);
+        }
+    }
+    storeChar(0, &caFrist, &caLast);
+    printfChar(caFrist, caLast, &iFlag);
+
+    __(") {\n");
+
+    _("    return -1;");
+    _("}");
+    _("iPos += 1;");
+    _("");
 }
 
-static int printfChar(unsigned char caChar)
+static int printfChar(unsigned char caFrist, unsigned char caLast, int *piFlag)
+{
+    char sFrist[6];
+    char sLast[6];
+
+    if ('\0' == caFrist) {
+        return 0;
+    }
+
+    if (*piFlag) {
+        __(" && ");
+    }
+
+    if (isprint(caFrist)) {
+        sprintf(sFrist, "'%c'", caFrist);
+    } else {
+        sprintf(sFrist, "0x%03d", caFrist);
+    }
+
+    *piFlag = 1;
+    if ('\0' == caLast) {
+        _("(psBuf[iPos] != %s)", sFrist);
+    } else {
+        if (isprint(caLast)) {
+            sprintf(sLast, "'%c'", caLast);
+        } else {
+            sprintf(sLast, "0x%03d", caLast);
+        }
+        __("(psBuf[iPos] < %s || psBuf[iPos] > %s)", sFrist, sLast);
+    }
+
+    return 0;
+}
+
+static int storeChar(unsigned char caChar, unsigned char *psFrist, unsigned char *psLast)
 {
     static unsigned char caFrist = 0;
     static unsigned char caLast = 0;
 
     if (0 == caChar) {
-        if (0 == caFrist) {
-            return 0;
-        }
-
-        if (isprint(caFrist)) {
-            printf("%c", caFrist);
-        } else {
-            printf("\\%03d", caFrist);
-        }
-
-        if (0 != caLast) {
-            if (caFrist + 1 != caLast) {
-                printf("-");
-            }
-
-            if (isprint(caLast)) {
-                printf("%c", caLast);
-            } else {
-                printf("\\%03d", caLast);
-            }
-        }
+        *psFrist = caFrist;
+        *psLast = caLast;
 
         caFrist = 0;
         caLast = 0;
@@ -323,6 +407,31 @@ static int printfChar(unsigned char caChar)
     caLast = caChar;
     
     return 0;
+}
+
+static int _s()
+{
+    int i = 0;
+    for (i=0; i<f_Level; i++) {
+        printf("\t");
+    }
+
+    return 0;
+}
+
+static int _(char *psBuf, ...)
+{
+    va_list	vaArgs;
+	int		iRet;
+
+    _s();
+
+	va_start(vaArgs, psBuf);
+	iRet = vprintf(psBuf, vaArgs);
+	va_end(vaArgs);
+    printf("\n");
+
+	return iRet;
 }
 
 /*-----------------------------  End ------------------------------------*/
